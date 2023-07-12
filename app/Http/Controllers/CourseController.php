cc16e38ea2c9fb0717728555f35111f14ef55e19
abianfikri <abronjoy46@gmail.com>
2023-07-12 23:59:52 +0700

    controller, create, view course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use App\Models\Lecture;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class CourseController extends Controller
 {
     public function index()
     {
-        $courses = Course::where('user_id', auth()->user()->id)
-                ->orderBy('created_at', 'desc')
-                ->get();
+        $courses = Course::orderBy('created_at', 'desc')
+            ->get();
 
         //dd($courses);
 
@@ -20,10 +21,27 @@ class CourseController extends Controller
     }
     public function create()
     {
-        return view('course.create');
+        $users = User::where('id', '!=', '1')->get();
+        $lectures = Lecture::get();
+        return view('course.create', compact('users', 'lectures'));
     }
     public function edit()
     {
         return view('course.edit');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|max:255',
+        ]);
+        Course::create([
+            'nama' => ucfirst($request->nama),
+            'user_id' => $request->user_id,
+            'semester' => ucfirst($request->semester),
+            'lecture_id' => $request->lecture_id,
+        ]);
+
+        return redirect()->route('course.index')->with('success', 'Course Created succesfully!');
     }
 }
--- resources/views/course/create.blade.php
+++ resources/views/course/create.blade.php
@@ -5,11 +5,54 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+    <div class="sm:py-12 h-screen">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white dark:bg-gray-800 sm:shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <form method="post" action="{{ route('course.store') }}" class="">
+                        @csrf
+                        @method('post')
+                        <div class="mb-6">
+                            <x-input-label for="nama" :value="__('Nama')" />
+                            <x-text-input id="nama" name="nama" type="text" class="block w-full mt-1" autofocus autocomplete="nama" :value="old('nama')" />
+                            <x-input-error class="mt-2" :messages="$errors->get('nama')" />
+                        </div>
+                        <div class="mb-6">
+                            <x-input-label for="semester" :value="__('Semester')" />
+                            <x-text-input id="semester" name="semester" type="text" class="block w-full mt-1" autofocus autocomplete="semester" :value="old('semester')" />
+                            <x-input-error class="mt-2" :messages="$errors->get('semester')" />
+                        </div>
+                        <div class="mb-6">
+                            <x-input-label for="user_id" :value="__('Nama Asdos')" />
+                            <x-select id="user_id" name="user_id" class="block w-full mt-1">
+                                <option value="">Empty</option>
+                                @foreach ($users as $category)
+                                <option value="{{ $category->id }}" {{ old('user_id')==$category->id ?
+                                    'selected' : '' }}>
+                                    {{ $category->name }}
+                                </option>
+                                @endforeach
+                            </x-select>
+                            <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
+                        </div>
+                        <div class="mb-6">
+                            <x-input-label for="lecture_id" :value="__('Nama Lecture')" />
+                            <x-select id="lecture_id" name="lecture_id" class="block w-full mt-1">
+                                <option value="">Empty</option>
+                                @foreach ($lectures as $category)
+                                <option value="{{ $category->id }}" {{ old('lecture_id')==$category->id ?
+                                    'selected' : '' }}>
+                                    {{ $category->nama }}
+                                </option>
+                                @endforeach
+                            </x-select>
+                            <x-input-error class="mt-2" :messages="$errors->get('category_id')" />
+                        </div>
+                        <div class="flex items-center gap-4">
+                            <x-primary-button>{{ __('Save') }}</x-primary-button>
+                            <x-cancel-button href="{{ route('course.index') }}" />
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
